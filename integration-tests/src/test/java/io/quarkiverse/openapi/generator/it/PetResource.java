71c6bedf6b682229b818f810b72cfdc4cc1f6f52
Ricardo Zanini <zanini@redhat.com>
2022-02-08 11:29:00 -0300

    Fix properties reading from codegen
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- integration-tests/src/test/java/io/quarkiverse/openapi/generator/it/PetResource.java
+++ integration-tests/src/test/java/io/quarkiverse/openapi/generator/it/PetResource.java
@@ -6,9 +6,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.acme.openapi.api.PetApi;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
-import org.openapitools.client.api.PetApi;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/petstore")
