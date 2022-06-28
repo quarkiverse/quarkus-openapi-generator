e5060092010f37791b18f1dba95abc4e293e1dd0
Francisco Javier Tirado Sarti <ftirados@redhat.com>
2022-06-28 18:20:14 +0200

    Review comments

--- deployment/src/test/java/io/quarkiverse/openapi/generator/deployment/CodegenConfigTest.java
+++ deployment/src/test/java/io/quarkiverse/openapi/generator/deployment/CodegenConfigTest.java
@@ -10,10 +10,17 @@ import org.junit.jupiter.api.Test;
 class CodegenConfigTest {
 
     @Test
-    void verifySpaceEncoding() {
+    void verifyStreamUri() {
         final String resolvedPrefix = CodegenConfig
                 .getBuildTimeSpecPropertyPrefix(Path.of("/home/myuser/open-api-stream/luke/my test openapi.json"));
         assertEquals(String.format(BUILD_TIME_SPEC_PREFIX_FORMAT, "luke_my_test_openapi_json"), resolvedPrefix);
+    }
+
+    @Test
+    void verifySpaceEncoding() {
+        final String resolvedPrefix = CodegenConfig
+                .getBuildTimeSpecPropertyPrefix(Path.of("/home/myuser/luke/my test openapi.json"));
+        assertEquals(String.format(BUILD_TIME_SPEC_PREFIX_FORMAT, "my_test_openapi_json"), resolvedPrefix);
     }
 
     @Test
