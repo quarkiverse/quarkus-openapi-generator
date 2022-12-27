ab87bf69e1d74d0226f5aa5f55281f55224f6581
Helber Belmiro <helber.belmiro@gmail.com>
2022-12-27 14:33:10 -0300

    issue-205 Added support to enum arrays (#211)

---
+++ integration-tests/array-enum/src/test/java/io/quarkiverse/openapi/generator/it/ArrayEnumTest.java
@@ -0,0 +1,24 @@
+package io.quarkiverse.openapi.generator.it;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Test;
+import org.openapi.quarkus.array_enum_yaml.api.ArrayEnumResourceApi;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class ArrayEnumTest {
+
+    @RestClient
+    @Inject
+    ArrayEnumResourceApi api;
+
+    @Test
+    void apiIsBeingGenerated() {
+        assertThat(api).isNotNull();
+    }
+}
