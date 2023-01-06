1e81a61856a267212a45e96d54edb258d608124c
Helber Belmiro <helber.belmiro@gmail.com>
2023-01-06 16:09:44 -0300

    Added "return-response" property to return `Response` instead of a model (#219)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

---
+++ integration-tests/return-response/src/test/java/io/quarkiverse/openapi/generator/it/ReturnResponseTest.java
@@ -0,0 +1,27 @@
+package io.quarkiverse.openapi.generator.it;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.ws.rs.core.Response;
+
+import org.acme.openapi.api.ModelApi;
+import org.acme.openapi.api.ResponseApi;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class ReturnResponseTest {
+
+    @Test
+    void testModel() throws NoSuchMethodException {
+        assertThat(ModelApi.class.getMethod("hello").getReturnType())
+                .isEqualTo(String.class);
+    }
+
+    @Test
+    void testResponse() throws NoSuchMethodException {
+        assertThat(ResponseApi.class.getMethod("hello").getReturnType())
+                .isEqualTo(Response.class);
+    }
+}
