3259f907f63521322bc8ee4f683e3c5e2dfa0bc9
Helber Belmiro <helber.belmiro@gmail.com>
2023-02-23 10:33:06 -0300

    Removed message about RESTEasy Reactive and Classic coexisting
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/codegen/RestEasyImplementationVerifier.java
+++ deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/codegen/RestEasyImplementationVerifier.java
@@ -6,8 +6,6 @@ final class RestEasyImplementationVerifier {
 
     private static final RestEasyImplementationVerifier INSTANCE = new RestEasyImplementationVerifier();
 
-    private static final String BOTH_DEPENDENCIES_PRESENT_ERROR_MSG = "The application has RESTEasy classic and reactive on its dependencies. Both dependencies cannot coexists. You need to remove one of them.";
-
     static final String RESTEASY_REACTIVE_ARTIFACT_ID = "resteasy-reactive";
 
     static final String RESTEASY_CLASSIC_ARTIFACT_ID = "quarkus-resteasy-common";
@@ -20,30 +18,15 @@ final class RestEasyImplementationVerifier {
     }
 
     boolean isRestEasyReactive(Collection<String> artifactIds) {
-        boolean foundReactive = false;
-        boolean foundClassic = false;
-
         for (String artifactId : artifactIds) {
             if (RESTEASY_REACTIVE_ARTIFACT_ID.equals(artifactId)) {
-                foundReactive = true;
-                if (foundClassic) {
-                    throw new IllegalStateException(BOTH_DEPENDENCIES_PRESENT_ERROR_MSG);
-                }
+                return true;
             } else if (RESTEASY_CLASSIC_ARTIFACT_ID.equals(artifactId)) {
-                foundClassic = true;
-                if (foundReactive) {
-                    throw new IllegalStateException(BOTH_DEPENDENCIES_PRESENT_ERROR_MSG);
-                }
+                return false;
             }
         }
 
-        if (foundReactive) {
-            return true;
-        } else if (foundClassic) {
-            return false;
-        } else {
-            throw new IllegalStateException(
-                    "It was not possible to identify if the application is using RESTEasy classic or reactive");
-        }
+        throw new IllegalStateException(
+                "It was not possible to identify if the application is using RESTEasy classic or reactive");
     }
 }
