ee8b78617fcd933deea8cc0f1d503e54b3e76829
Christian Berger <chberger@users.noreply.github.com>
2023-03-10 14:11:27 +0100

    feat: Bump org.openapitools to 6.4.0

--- deployment/src/test/java/io/quarkiverse/openapi/generator/deployment/template/QuteTemplatingEngineAdapterTest.java
+++ deployment/src/test/java/io/quarkiverse/openapi/generator/deployment/template/QuteTemplatingEngineAdapterTest.java
@@ -2,6 +2,7 @@ package io.quarkiverse.openapi.generator.deployment.template;
 
 import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
@@ -10,6 +11,8 @@ import java.nio.file.Files;
 import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.openapitools.codegen.DefaultGenerator;
 import org.openapitools.codegen.config.CodegenConfigurator;
 
@@ -36,5 +39,21 @@ public class QuteTemplatingEngineAdapterTest {
         } else {
             fail("Template failed to write to the file");
         }
+    }
+
+    @Test
+    @EnabledOnOs({ OS.WINDOWS })
+    void checkOpenApiSpecIsNotAvailableOnWindows() throws IOException {
+        // On Windows getResource prepends a leading slash which leads to an invalid file path
+        final String petstoreOpenApi = requireNonNull(this.getClass().getResource("/openapi/petstore-openapi.json"))
+                .getPath();
+        final DefaultGenerator generator = new DefaultGenerator();
+        final CodegenConfigurator configurator = new QuarkusCodegenConfigurator();
+        final File apiFile = File.createTempFile("api", ".java");
+        apiFile.deleteOnExit();
+        configurator.setInputSpec(petstoreOpenApi);
+        // skip message evaluation as the NPE doesn't have a message in all scenarios, seems to be JDK dependent
+        assertThrows(NullPointerException.class,
+                () -> generator.opts(configurator.toClientOptInput()));
     }
 }
