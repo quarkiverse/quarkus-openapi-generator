bf8489d6c06998b3d39c9ba6b4c45e82524e0da3
Helber Belmiro <helber.belmiro@gmail.com>
2023-04-28 16:05:09 -0300

    Replaced @QueryParams with @BeanParams (#313)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>
    (cherry picked from commit 913ee37b4b364b4749dcb9cf1870eb1770fbe73e)

---
+++ integration-tests/beanparam/src/test/java/io/quarkiverse/openapi/generator/it/WiremockBeanParam.java
@@ -0,0 +1,46 @@
+package io.quarkiverse.openapi.generator.it;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+
+import java.util.Map;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+public class WiremockBeanParam implements QuarkusTestResourceLifecycleManager {
+
+    public static final String URL_KEY = "quarkus.rest-client.openapi_yaml.url";
+
+    private WireMockServer wireMockServer;
+
+    @Override
+    public Map<String, String> start() {
+        wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort());
+        wireMockServer.start();
+
+        wireMockServer.stubFor(get(urlPathEqualTo("/get"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(
+                                "{\"message\": \"Hello\"}")));
+
+        return Map.of(URL_KEY, wireMockServer.baseUrl());
+    }
+
+    @Override
+    public void inject(TestInjector testInjector) {
+        testInjector.injectIntoFields(wireMockServer, f -> f.getName().equals("wireMockServer"));
+    }
+
+    @Override
+    public void stop() {
+        if (null != wireMockServer) {
+            wireMockServer.stop();
+        }
+    }
+}
