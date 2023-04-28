24d4977811e7a4192bcdca1f02eae360f203dda1
Helber Belmiro <helber.belmiro@gmail.com>
2023-04-28 16:34:44 -0300

    Replaced @QueryParams with @BeanParams (#313) (#317)
    * Replaced @QueryParams with @BeanParams (#313)
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>
    (cherry picked from commit 913ee37b4b364b4749dcb9cf1870eb1770fbe73e)
    
    * Replaced jakarta with javax
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>
    
    ---------
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

---
+++ integration-tests/beanparam/src/test/java/io/quarkiverse/openapi/generator/it/BeanParamOpenApiTest.java
@@ -0,0 +1,40 @@
+package io.quarkiverse.openapi.generator.it;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Test;
+import org.openapi.quarkus.openapi_yaml.api.GetTestApi;
+import org.openapi.quarkus.openapi_yaml.model.ResponseDto;
+import org.openapi.quarkus.openapi_yaml.model.TestObj;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTestResource(WiremockBeanParam.class)
+@QuarkusTest
+class BeanParamOpenApiTest {
+
+    @RestClient
+    @Inject
+    GetTestApi api;
+
+    WireMockServer wireMockServer;
+
+    @Test
+    void apiIsBeingGenerated() {
+        TestObj model = new TestObj();
+        model.size(42);
+
+        ResponseDto responseDto = api.getTest(model, true);
+        assertThat(responseDto.getMessage()).isEqualTo("Hello");
+
+        wireMockServer.verify(WireMock.getRequestedFor(
+                WireMock.urlEqualTo("/get?size=42&unpaged=true")));
+    }
+}
