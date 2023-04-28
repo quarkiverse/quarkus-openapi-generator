78c9a797504ef2192d20432fdedf2deaae20462f
Helber Belmiro <helber.belmiro@gmail.com>
2023-04-28 16:25:23 -0300

    Replaced jakarta with javax
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- integration-tests/beanparam/src/test/java/io/quarkiverse/openapi/generator/it/BeanParamOpenApiTest.java
+++ integration-tests/beanparam/src/test/java/io/quarkiverse/openapi/generator/it/BeanParamOpenApiTest.java
@@ -2,7 +2,7 @@ package io.quarkiverse.openapi.generator.it;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import jakarta.inject.Inject;
+import javax.inject.Inject;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.Test;
