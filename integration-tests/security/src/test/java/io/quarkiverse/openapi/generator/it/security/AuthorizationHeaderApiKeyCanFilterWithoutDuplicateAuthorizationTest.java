b13d5f98b922fa0951a41a6d3814297c873896a9
Laurent Perez <laurent.perez@itk.fr>
2023-05-02 17:08:11 +0200

    #308 use LoggedRequest case. remove comments.

--- integration-tests/security/src/test/java/io/quarkiverse/openapi/generator/it/security/AuthorizationHeaderApiKeyCanFilterWithoutDuplicateAuthorizationTest.java
+++ integration-tests/security/src/test/java/io/quarkiverse/openapi/generator/it/security/AuthorizationHeaderApiKeyCanFilterWithoutDuplicateAuthorizationTest.java
@@ -1,10 +1,14 @@
 package io.quarkiverse.openapi.generator.it.security;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 
+import com.github.tomakehurst.wiremock.verification.LoggedRequest;
 import jakarta.inject.Inject;
 
 import org.acme.openapi.foo.api.FooResourceApi;
@@ -13,7 +17,6 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.Test;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
@@ -33,10 +36,14 @@ public class AuthorizationHeaderApiKeyCanFilterWithoutDuplicateAuthorizationTest
         List<FooDTO> foos = fooResourceApi.getFoosUsingGET("not the fooapikey",
                 123465L);
         assertNotNull(foos);
-        fooServer.verify(WireMock.getRequestedFor(
-                WireMock.urlEqualTo("/api/foo/v2.0/foo?something=123465"))
+        fooServer.verify(getRequestedFor(
+                urlEqualTo("/api/foo/v2.0/foo?something=123465"))
                 .withHeader("Authorization", equalTo("fooapikey"))
-
         );
+
+        List<LoggedRequest> requestsWithAuthHeader = fooServer.findAll(getRequestedFor(
+                urlEqualTo("/api/foo/v2.0/foo?something=123465"))
+                .withHeader("Authorization", equalTo("fooapikey")));
+        assertEquals(1, requestsWithAuthHeader.size());
     }
 }
