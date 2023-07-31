68d1192a74b69f713a6a582749711c884e7a825b
Maximilian Zellhofer <max.zellhofer@gmail.com>
2023-07-31 11:09:54 +0200

    Return `Response` instead of `void` (#386)

--- integration-tests/return-response/src/test/java/io/quarkiverse/openapi/generator/it/ReturnResponseTest.java
+++ integration-tests/return-response/src/test/java/io/quarkiverse/openapi/generator/it/ReturnResponseTest.java
@@ -4,10 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import jakarta.ws.rs.core.Response;
 
-import org.acme.openapi.api.ReturnResponseFalseStringApi;
-import org.acme.openapi.api.ReturnResponseFalseVoidApi;
-import org.acme.openapi.api.ReturnResponseTrueStringApi;
-import org.acme.openapi.api.ReturnResponseTrueVoidApi;
+import org.acme.openapi.api.*;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -17,25 +14,57 @@ class ReturnResponseTest {
 
     @Test
     void testReturnResponseFalseString() throws NoSuchMethodException {
-        assertThat(ReturnResponseFalseStringApi.class.getMethod("hello").getReturnType())
+        var method = ReturnResponseFalseStringApi.class.getMethod("hello");
+        assertThat(method.getReturnType())
                 .isEqualTo(String.class);
     }
 
     @Test
     void testReturnResponseTrueString() throws NoSuchMethodException {
-        assertThat(ReturnResponseTrueStringApi.class.getMethod("hello").getReturnType())
+        var method = ReturnResponseTrueStringApi.class.getMethod("hello");
+        assertThat(method.getReturnType())
                 .isEqualTo(Response.class);
     }
 
     @Test
     void testReturnResponseFalseVoid() throws NoSuchMethodException {
-        assertThat(ReturnResponseFalseVoidApi.class.getMethod("hello").getReturnType())
-                .isEqualTo(Void.TYPE);
+        var method = ReturnResponseFalseVoidApi.class.getMethod("hello");
+        assertThat(method.getReturnType())
+                .isEqualTo(Response.class);
     }
 
     @Test
     void testReturnResponseTrueVoid() throws NoSuchMethodException {
-        assertThat(ReturnResponseTrueVoidApi.class.getMethod("hello").getReturnType())
+        var method = ReturnResponseTrueVoidApi.class.getMethod("hello");
+        assertThat(method.getReturnType())
                 .isEqualTo(Response.class);
+    }
+
+    @Test
+    void testMutinyReturnResponseFalseString() throws NoSuchMethodException {
+        var method = MutinyReturnResponseFalseStringApi.class.getMethod("hello");
+        assertThat(method.getGenericReturnType().getTypeName())
+                .isEqualTo("io.smallrye.mutiny.Uni<java.lang.String>");
+    }
+
+    @Test
+    void testMutinyReturnResponseTrueString() throws NoSuchMethodException {
+        var method = MutinyReturnResponseTrueStringApi.class.getMethod("hello");
+        assertThat(method.getGenericReturnType().getTypeName())
+                .isEqualTo("io.smallrye.mutiny.Uni<jakarta.ws.rs.core.Response>");
+    }
+
+    @Test
+    void testMutinyReturnResponseFalseVoid() throws NoSuchMethodException {
+        var method = MutinyReturnResponseFalseVoidApi.class.getMethod("hello");
+        assertThat(method.getGenericReturnType().getTypeName())
+                .isEqualTo("io.smallrye.mutiny.Uni<jakarta.ws.rs.core.Response>");
+    }
+
+    @Test
+    void testMutinyReturnResponseTrueVoid() throws NoSuchMethodException {
+        var method = MutinyReturnResponseTrueVoidApi.class.getMethod("hello");
+        assertThat(method.getGenericReturnType().getTypeName())
+                .isEqualTo("io.smallrye.mutiny.Uni<jakarta.ws.rs.core.Response>");
     }
 }
