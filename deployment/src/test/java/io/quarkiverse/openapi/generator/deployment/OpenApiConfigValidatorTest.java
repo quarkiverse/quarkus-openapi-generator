b34c7a68cddbf02cc40eec50592e82b3cabaad02
Michal Somora <10022003+michalsomora@users.noreply.github.com>
2023-08-09 16:47:02 +0200

    Update deployment/src/test/java/io/quarkiverse/openapi/generator/deployment/OpenApiConfigValidatorTest.java
    Co-authored-by: Helber Belmiro <helber.belmiro@gmail.com>

--- deployment/src/test/java/io/quarkiverse/openapi/generator/deployment/OpenApiConfigValidatorTest.java
+++ deployment/src/test/java/io/quarkiverse/openapi/generator/deployment/OpenApiConfigValidatorTest.java
@@ -1,45 +1,38 @@
 package io.quarkiverse.openapi.generator.deployment;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 import java.util.List;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import io.quarkiverse.openapi.generator.deployment.codegen.OpenApiConfigValidator;
 import io.quarkus.bootstrap.prebuild.CodeGenException;
 
-public class OpenApiConfigValidatorTest {
+class OpenApiConfigValidatorTest {
 
-    @Test
-    public void test_known_configs_ok() throws CodeGenException {
-        OpenApiConfigValidator
-                .validateInputConfiguration(List.of("quarkus.openapi-generator.codegen.additional-api-type-annotations"));
-        OpenApiConfigValidator.validateInputConfiguration(
-                List.of("quarkus.openapi-generator.codegen.spec.spec_yaml.enable-security-generation"));
-        OpenApiConfigValidator.validateInputConfiguration(
-                List.of("quarkus.openapi-generator.codegen.type-mappings.UUID=String"));
-        OpenApiConfigValidator.validateInputConfiguration(
-                List.of("quarkus.openapi-generator.codegen.spec.spec_yaml.type-mappings.UUID=String"));
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "quarkus.openapi-generator.codegen.additional-api-type-annotations",
+            "quarkus.openapi-generator.codegen.spec.spec_yaml.enable-security-generation",
+            "quarkus.openapi-generator.codegen.type-mappings.UUID=String",
+            "quarkus.openapi-generator.codegen.spec.spec_yaml.type-mappings.UUID=String"
+    })
+    void test_known_configs_ok(String validConfiguration) {
+        assertThatCode(() -> OpenApiConfigValidator.validateInputConfiguration(List.of(validConfiguration)))
+                .doesNotThrowAnyException();
     }
 
-    @Test
-    public void test_uknown_config_typo() {
-        CodeGenException codeGenException = Assertions.assertThrows(CodeGenException.class, () -> {
-            OpenApiConfigValidator
-                    .validateInputConfiguration(List.of("quarkus.openapi-generator.codegen.eenable-security-generation"));
-        });
-
-        Assertions.assertTrue(
-                codeGenException.getMessage().startsWith("Found unsupported configuration: [eenable-security-generation]"),
-                codeGenException.getMessage());
-
-        codeGenException = Assertions.assertThrows(CodeGenException.class, () -> {
-            OpenApiConfigValidator.validateInputConfiguration(
-                    List.of("quarkus.openapi-generator.codegen.spec.spec_yaml.eenable-security-generation"));
-        });
-
-        Assertions.assertTrue(
-                codeGenException.getMessage().startsWith("Found unsupported configuration: [eenable-security-generation]"),
-                codeGenException.getMessage());
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "quarkus.openapi-generator.codegen.eenable-security-generation",
+            "quarkus.openapi-generator.codegen.spec.spec_yaml.eenable-security-generation" })
+    void test_unknown_config_typo(String invalidConfiguration) {
+        assertThatExceptionOfType(CodeGenException.class)
+                .isThrownBy(() -> OpenApiConfigValidator
+                        .validateInputConfiguration(List.of(invalidConfiguration)))
+                .withMessageStartingWith("Found unsupported configuration: [eenable-security-generation]");
     }
 }
