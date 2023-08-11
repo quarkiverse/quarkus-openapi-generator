ad2e11ae54b483762ebef4eaaea4d8f61010c14f
Helber Belmiro <helber.belmiro@gmail.com>
2023-08-11 14:56:58 -0300

    Added tests for token propagation (#433)
    * Added tests for token propagation
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>
    
    ---------
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

---
+++ integration-tests/security/src/main/java/io/quarkiverse/openapi/generator/it/security/TokenPropagationResource.java
@@ -0,0 +1,60 @@
+package io.quarkiverse.openapi.generator.it.security;
+
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+@Path("/token_propagation")
+public class TokenPropagationResource {
+
+    @RestClient
+    org.acme.externalservice1.api.DefaultApi defaultApi1;
+
+    @RestClient
+    org.acme.externalservice2.api.DefaultApi defaultApi2;
+
+    @RestClient
+    org.acme.externalservice3.api.DefaultApi defaultApi3;
+
+    @RestClient
+    org.acme.externalservice4.api.DefaultApi defaultApi4;
+
+    @RestClient
+    org.acme.externalservice5.api.DefaultApi defaultApi5;
+
+    @POST
+    @Path("service1")
+    public String service1() {
+        defaultApi1.executeQuery1();
+        return "hello";
+    }
+
+    @POST
+    @Path("service2")
+    public String service2() {
+        defaultApi2.executeQuery2();
+        return "hello";
+    }
+
+    @POST
+    @Path("service3")
+    public String service3() {
+        defaultApi3.executeQuery3();
+        return "hello";
+    }
+
+    @POST
+    @Path("service4")
+    public String service4() {
+        defaultApi4.executeQuery4();
+        return "hello";
+    }
+
+    @POST
+    @Path("service5")
+    public String service5() {
+        defaultApi5.executeQuery5();
+        return "hello";
+    }
+}
