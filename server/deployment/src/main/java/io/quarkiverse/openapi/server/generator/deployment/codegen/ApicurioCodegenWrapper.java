2e887a2c8455b7ce5599d1c44aad9f1ae1b63bcf
Carles Arnal <carlesarnal92@gmail.com>
2023-10-09 17:41:39 +0200

    Create client directory

--- server/deployment/src/main/java/io/quarkiverse/openapi/server/generator/deployment/codegen/ApicurioCodegenWrapper.java
+++ server/deployment/src/main/java/io/quarkiverse/openapi/server/generator/deployment/codegen/ApicurioCodegenWrapper.java
@@ -1,21 +1,25 @@
 package io.quarkiverse.openapi.server.generator.deployment.codegen;
 
-import static io.quarkiverse.openapi.server.generator.deployment.CodegenConfig.getBasePackagePropertyName;
-
-import java.io.*;
-import java.nio.file.Path;
-import java.util.Enumeration;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-
+import io.apicurio.hub.api.codegen.JaxRsProjectSettings;
+import io.apicurio.hub.api.codegen.OpenApi2JaxRs;
+import io.quarkus.bootstrap.prebuild.CodeGenException;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.microprofile.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.apicurio.hub.api.codegen.JaxRsProjectSettings;
-import io.apicurio.hub.api.codegen.OpenApi2JaxRs;
-import io.quarkus.bootstrap.prebuild.CodeGenException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import static io.quarkiverse.openapi.server.generator.deployment.CodegenConfig.getBasePackagePropertyName;
 
 public class ApicurioCodegenWrapper {
 
@@ -53,16 +57,20 @@ public class ApicurioCodegenWrapper {
 
         // Generate code - output a ZIP file.
         File zipFile = new File(outdir, "generated-code.zip");
-        try (FileOutputStream fos = new FileOutputStream(zipFile)) {
+
+        try (FileOutputStream fos = new FileOutputStream(zipFile);
+                FileInputStream openApiStream = new FileInputStream(openApiFile)) {
             OpenApi2JaxRs generator = new OpenApi2JaxRs();
             generator.setSettings(projectSettings);
             generator.setUpdateOnly(true);
-            generator.setOpenApiDocument(new FileInputStream(openApiFile));
+            generator.setOpenApiDocument(openApiStream);
             log.info("Generating code...");
             generator.generate(fos);
         } catch (Exception e) {
             log.error("Error generating code from openapi spec", e);
             throw new CodeGenException(e);
+        } finally {
+
         }
 
         // Unpack the temporary ZIP file
