701e455dc201265c9093024ec70fcb0686779339
Rupert Bogensperger <rupertbogensperger21@gmail.com>
2024-03-11 20:17:03 +0100

    added support for cookies

--- client/deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/template/QuteTemplatingEngineAdapter.java
+++ client/deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/template/QuteTemplatingEngineAdapter.java
@@ -1,8 +1,10 @@
 package io.quarkiverse.openapi.generator.deployment.template;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
+import org.openapitools.codegen.CodegenOperation;
 import org.openapitools.codegen.api.AbstractTemplatingEngineAdapter;
 import org.openapitools.codegen.api.TemplatingExecutor;
 
@@ -14,35 +16,36 @@ public class QuteTemplatingEngineAdapter extends AbstractTemplatingEngineAdapter
 
     public static final String IDENTIFIER = "qute";
     public static final String[] INCLUDE_TEMPLATES = {
-            "additionalEnumTypeAnnotations.qute",
-            "additionalModelTypeAnnotations.qute",
-            "beanValidation.qute",
-            "beanValidationCore.qute",
-            "beanValidationHeaderParams.qute",
-            "bodyParams.qute",
-            "enumClass.qute",
-            "enumOuterClass.qute",
-            "headerParams.qute",
-            "pathParams.qute",
-            "pojo.qute",
-            "pojoQueryParam.qute",
-            "queryParams.qute",
-            "auth/compositeAuthenticationProvider.qute",
-            "auth/headersFactory.qute",
-            "multipartFormdataPojo.qute",
-            "pojoAdditionalProperties.qute",
-            "operationJavaDoc.qute"
+        "additionalEnumTypeAnnotations.qute",
+        "additionalModelTypeAnnotations.qute",
+        "beanValidation.qute",
+        "beanValidationCore.qute",
+        "beanValidationHeaderParams.qute",
+        "bodyParams.qute",
+        "enumClass.qute",
+        "enumOuterClass.qute",
+        "headerParams.qute",
+        "pathParams.qute",
+        "cookieParams.qute",
+        "pojo.qute",
+        "pojoQueryParam.qute",
+        "queryParams.qute",
+        "auth/compositeAuthenticationProvider.qute",
+        "auth/headersFactory.qute",
+        "multipartFormdataPojo.qute",
+        "pojoAdditionalProperties.qute",
+        "operationJavaDoc.qute"
     };
     public final Engine engine;
 
     public QuteTemplatingEngineAdapter() {
         this.engine = Engine.builder()
-                .addDefaults()
-                .addValueResolver(new ReflectionValueResolver())
-                .addNamespaceResolver(OpenApiNamespaceResolver.INSTANCE)
-                .removeStandaloneLines(true)
-                .strictRendering(false)
-                .build();
+            .addDefaults()
+            .addValueResolver(new ReflectionValueResolver())
+            .addNamespaceResolver(OpenApiNamespaceResolver.INSTANCE)
+            .removeStandaloneLines(true)
+            .strictRendering(false)
+            .build();
     }
 
     @Override
@@ -52,19 +55,31 @@ public class QuteTemplatingEngineAdapter extends AbstractTemplatingEngineAdapter
 
     @Override
     public String[] getFileExtensions() {
-        return new String[] { IDENTIFIER };
+        return new String[]{IDENTIFIER};
     }
 
     @Override
-    public String compileTemplate(TemplatingExecutor executor, Map<String, Object> bundle, String templateFile)
-            throws IOException {
+    public String compileTemplate(TemplatingExecutor executor,
+        Map<String, Object> bundle,
+        String templateFile)
+        throws IOException {
         this.cacheTemplates(executor);
+
         Template template = engine.getTemplate(templateFile);
         if (template == null) {
             template = engine.parse(executor.getFullTemplateContents(templateFile));
-            engine.putTemplate(templateFile, template);
+            engine.putTemplate(templateFile,
+                template);
         }
-        return template.data(bundle).render();
+        if (templateFile.equals("api.qute")) {
+            ((List) ((Map) bundle.get("operations")).get("operation")).forEach(operation -> {
+                if (((CodegenOperation) operation).path.contains("/findByStatus")) {
+                    System.out.println(operation);
+                }
+            });
+        }
+        return template.data(bundle)
+            .render();
     }
 
     public void cacheTemplates(TemplatingExecutor executor) {
@@ -72,7 +87,8 @@ public class QuteTemplatingEngineAdapter extends AbstractTemplatingEngineAdapter
             Template incTemplate = engine.getTemplate(templateId);
             if (incTemplate == null) {
                 incTemplate = engine.parse(executor.getFullTemplateContents(templateId));
-                engine.putTemplate(templateId, incTemplate);
+                engine.putTemplate(templateId,
+                    incTemplate);
             }
         }
     }
