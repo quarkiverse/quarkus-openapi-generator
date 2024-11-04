aa9f471cb402c3a2c6a2600c093076dc2e97a3f8
Loïc Hermann <loic.hermann@outlook.fr>
2024-11-04 15:34:12 -0500

    fix Build fails if quarkus-oidc-client is not in the classpath (#829)

---
+++ client/integration-tests/without-oidc/src/test/java/io/quarkiverse/openapi/generator/it/QuarkusSimpleOpenApiTest.java
@@ -0,0 +1,24 @@
+package io.quarkiverse.openapi.generator.it;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.inject.Inject;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Test;
+import org.openapi.quarkus.quarkus_simple_openapi_yaml.api.ReactiveGreetingResourceApi;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class QuarkusSimpleOpenApiTest {
+
+    @RestClient
+    @Inject
+    ReactiveGreetingResourceApi api;
+
+    @Test
+    void apiIsBeingGenerated() {
+        assertThat(api).isNotNull();
+    }
+}
