840e2917e68373b11fd507233d832a7c2cfebbd2
yuhaibo <yuhaibo@hotmail.com>
2024-11-27 15:03:18 +0800

    add integration-test for remove operation id prefix

---
+++ client/integration-tests/remove-operationid-prefix/src/test/java/io/quarkiverse/openapi/generator/it/RemoveOperatiionIdPrefixTest.java
@@ -0,0 +1,33 @@
+package io.quarkiverse.openapi.generator.it;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class RemoveOperatiionIdPrefixTest {
+
+    String apiClassName = "org.openapi.quarkus.openapi_remove_operation_id_prefix_yaml.api.UserResourceApi";
+    String modelClassName = "org.openapi.quarkus.openapi_remove_operation_id_prefix_yaml.model.User";
+
+    @Test
+    void apiIsBeingGenerated() throws NoSuchMethodException {
+        assertThatCode(() -> Class.forName(apiClassName).getMethod("find", Integer.class))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> Class.forName(apiClassName).getMethod("findAll"))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> Class.forName(apiClassName).getMethod("add", Class.forName(modelClassName)))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> Class.forName(apiClassName).getMethod("update", Integer.class, Class.forName(modelClassName)))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> Class.forName(apiClassName).getMethod("delete", Integer.class))
+                .doesNotThrowAnyException();
+
+    }
+}
