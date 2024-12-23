ae3a06e073cc8b512250b571ddca10a373252d50
Francisco Javier Tirado Sarti <65240126+fjtirado@users.noreply.github.com>
2024-12-23 16:32:29 +0100

    Merge pull request #920 from denvitaharen/feature/add-equals-hashcode-client
    Added so equals and hashcode is generated in models.

---
+++ client/integration-tests/equals-hashcode/src/test/java/io/quarkiverse/openapi/generator/it/VerifyGenerationTest.java
@@ -0,0 +1,42 @@
+package io.quarkiverse.openapi.generator.it;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class VerifyGenerationTest {
+
+    @Test
+    void verifyThatGeneratedModelDoesHaveMethods() {
+        var equalsMethod = getMethod(org.acme.equals.hashcode.model.Animal.class, "equals");
+        var hashCodeMethod = getMethod(org.acme.equals.hashcode.model.Animal.class, "hashCode");
+
+        assertEquals(equalsMethod.getDeclaringClass(), org.acme.equals.hashcode.model.Animal.class);
+        assertEquals(hashCodeMethod.getDeclaringClass(), org.acme.equals.hashcode.model.Animal.class);
+    }
+
+    @Test
+    void verifyThatGeneratedModelDoesntHaveMethods() {
+        var equalsMethod = getMethod(org.acme.non.equals.hashcode.model.Animal.class, "equals");
+        var hashCodeMethod = getMethod(org.acme.non.equals.hashcode.model.Animal.class, "hashCode");
+
+        assertNotEquals(equalsMethod.getDeclaringClass(), org.acme.equals.hashcode.model.Animal.class);
+        assertNotEquals(hashCodeMethod.getDeclaringClass(), org.acme.equals.hashcode.model.Animal.class);
+    }
+
+    private static Method getMethod(Class<?> clazz, String methodName) {
+        var methods = clazz.getMethods();
+
+        return Arrays.stream(methods)
+                .filter(method -> method.getName().equals(methodName))
+                .findAny()
+                .orElseThrow();
+    }
+}
