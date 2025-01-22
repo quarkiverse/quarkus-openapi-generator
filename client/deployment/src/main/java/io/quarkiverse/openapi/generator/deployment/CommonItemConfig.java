a940d8239770ce3272200c2d4edca62e0f1dab92
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2025-01-22 09:40:02 -0300

    Convert client and server modules to use @ConfigMapping (#944)

--- client/deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/CommonItemConfig.java
+++ client/deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/CommonItemConfig.java
@@ -3,8 +3,7 @@ package io.quarkiverse.openapi.generator.deployment;
 import java.util.Map;
 import java.util.Optional;
 
-import io.quarkus.runtime.annotations.ConfigGroup;
-import io.quarkus.runtime.annotations.ConfigItem;
+import io.smallrye.config.WithName;
 
 /*
  * Model for the configuration of this extension.
@@ -13,92 +12,91 @@ import io.quarkus.runtime.annotations.ConfigItem;
  * Not meant to be used outside this scope.
  * Config items can be applied on spec and globally as well
  */
-@ConfigGroup
-public class CommonItemConfig {
+public interface CommonItemConfig {
 
     /**
      * Whether to skip the generation of models for form parameters
      */
-    @ConfigItem(name = "skip-form-model")
-    public Optional<Boolean> skipFormModel;
+    @WithName("skip-form-model")
+    Optional<Boolean> skipFormModel();
 
     /**
      * Type Mapping is an OpenAPI Generator configuration specifying which Java types (the values) should be used for a
      * given OAS datatype (the keys of this map)
      */
-    @ConfigItem(name = "type-mappings")
-    public Map<String, String> typeMappings;
+    @WithName("type-mappings")
+    Map<String, String> typeMappings();
 
     /**
      * Import Mapping is an OpenAPI Generator configuration specifying which Java types (the values) should be
      * imported when a given OAS datatype (the keys of this map) is used
      */
-    @ConfigItem(name = "import-mappings")
-    public Map<String, String> importMappings;
+    @WithName("import-mappings")
+    Map<String, String> importMappings();
 
     /**
      * Schema Mapping is an OpenAPI Generator configuration specifying which Java types (the values) should be
      * imported when a given schema type (the keys of this map) is used
      */
-    @ConfigItem(name = "schema-mappings")
-    public Map<String, String> schemaMappings;
+    @WithName("schema-mappings")
+    Map<String, String> schemaMappings();
 
     /**
      * The specified annotations will be added to the generated model files
      */
-    @ConfigItem(name = "additional-model-type-annotations")
-    public Optional<String> additionalModelTypeAnnotations;
+    @WithName("additional-model-type-annotations")
+    Optional<String> additionalModelTypeAnnotations();
 
     /**
      * Defines if the enums should have an `UNEXPECTED` member to convey values that cannot be parsed. Default is
      * {@code false}.
      */
-    @ConfigItem(name = "additional-enum-type-unexpected-member")
-    public Optional<Boolean> additionalEnumTypeUnexpectedMemberAnnotations;
+    @WithName("additional-enum-type-unexpected-member")
+    Optional<Boolean> additionalEnumTypeUnexpectedMemberAnnotations();
 
     /**
      * The specified annotations will be added to the generated api files
      */
-    @ConfigItem(name = "additional-api-type-annotations")
-    public Optional<String> additionalApiTypeAnnotations;
+    @WithName("additional-api-type-annotations")
+    Optional<String> additionalApiTypeAnnotations();
 
     /**
      * Add custom/additional HTTP Headers or other args to every request
      */
-    @ConfigItem(name = "additional-request-args")
-    public Optional<String> additionalRequestArgs;
+    @WithName("additional-request-args")
+    Optional<String> additionalRequestArgs();
 
     /**
      * Defines if the methods should return {@link jakarta.ws.rs.core.Response} or a model. Default is {@code false}.
      */
-    @ConfigItem(name = "return-response")
-    public Optional<Boolean> returnResponse;
+    @WithName("return-response")
+    Optional<Boolean> returnResponse();
 
     /**
      * Defines if security support classes should be generated
      */
-    @ConfigItem(name = "enable-security-generation")
-    public Optional<String> enableSecurityGeneration;
+    @WithName("enable-security-generation")
+    Optional<String> enableSecurityGeneration();
 
     /**
      * Defines the normalizer options.
      */
-    @ConfigItem(name = "open-api-normalizer")
-    public Map<String, String> normalizer;
+    @WithName("open-api-normalizer")
+    Map<String, String> normalizer();
 
     /**
      * Enable SmallRye Mutiny support. If you set this to {@code true}, all return types will be wrapped in
      * {@link io.smallrye.mutiny.Uni}.
      */
-    @ConfigItem(name = "mutiny")
-    public Optional<Boolean> supportMutiny;
+    @WithName("mutiny")
+    Optional<Boolean> supportMutiny();
 
     /**
      * Defines with SmallRye Mutiny enabled if methods should return {@link jakarta.ws.rs.core.Response} or a model. Default is
      * {@code false}.
      */
-    @ConfigItem(name = "mutiny.return-response")
-    public Optional<Boolean> mutinyReturnResponse;
+    @WithName("mutiny.return-response")
+    Optional<Boolean> mutinyReturnResponse();
 
     /**
      * Handles the return type for each operation, depending on the configuration.
@@ -125,16 +123,16 @@ public class CommonItemConfig {
      * - If the operation has a void return type, it will return {@link io.smallrye.mutiny.Uni<jakarta.ws.rs.core.Response>}.
      * - Otherwise, it will return {@link io.smallrye.mutiny.Uni<returnType>}`.
      */
-    @ConfigItem(name = "mutiny.operation-ids")
-    public Optional<Map<String, String>> mutinyMultiOperationIds;
+    @WithName("mutiny.operation-ids")
+    Map<String, String> mutinyMultiOperationIds();
 
     /**
      * Defines, whether the `PartFilename` ({@link org.jboss.resteasy.reactive.PartFilename} or
      * {@link org.jboss.resteasy.annotations.providers.multipart.PartFilename}) annotation should be generated for
      * MultipartForm POJOs. By setting to {@code false}, the annotation will not be generated.
      */
-    @ConfigItem(name = "generate-part-filename")
-    public Optional<Boolean> generatePartFilename;
+    @WithName("generate-part-filename")
+    Optional<Boolean> generatePartFilename();
 
     /**
      * Defines the filename for a part in case the `PartFilename` annotation
@@ -143,40 +141,40 @@ public class CommonItemConfig {
      * In case no value is set, the default one is `&lt;fieldName&gt;File` or `file`, depending on the
      * {@link CommonItemConfig#useFieldNameInPartFilename} configuration.
      */
-    @ConfigItem(name = "part-filename-value")
-    public Optional<String> partFilenameValue;
+    @WithName("part-filename-value")
+    Optional<String> partFilenameValue();
 
     /**
      * Defines, whether the filename should also include the property name in case the `PartFilename` annotation
      * ({@link org.jboss.resteasy.reactive.PartFilename} or
      * {@link org.jboss.resteasy.annotations.providers.multipart.PartFilename}) is generated.
      */
-    @ConfigItem(name = "use-field-name-in-part-filename")
-    public Optional<Boolean> useFieldNameInPartFilename;
+    @WithName("use-field-name-in-part-filename")
+    Optional<Boolean> useFieldNameInPartFilename();
 
     /**
      * Enable bean validation. If you set this to {@code true}, validation annotations are added to generated sources E.g.
      * {@code @Size}.
      */
-    @ConfigItem(name = "use-bean-validation")
-    public Optional<Boolean> useBeanValidation;
+    @WithName("use-bean-validation")
+    Optional<Boolean> useBeanValidation();
 
     /**
      * Enable the generation of APIs. If you set this to {@code false}, APIs will not be generated.
      */
-    @ConfigItem(name = "generate-apis")
-    public Optional<Boolean> generateApis;
+    @WithName("generate-apis")
+    Optional<Boolean> generateApis();
 
     /**
      * Enable the generation of models. If you set this to {@code false}, models will not be generated.
      */
-    @ConfigItem(name = "generate-models")
-    public Optional<Boolean> generateModels;
+    @WithName("generate-models")
+    Optional<Boolean> generateModels();
 
     /**
      * Enable the generation of equals and hashcode in models. If you set this to {@code false}, the models
      * will not have equals and hashcode.
      */
-    @ConfigItem(name = "equals-hashcode")
-    public Optional<Boolean> equalsHashcode;
+    @WithName("equals-hashcode")
+    Optional<Boolean> equalsHashcode();
 }
