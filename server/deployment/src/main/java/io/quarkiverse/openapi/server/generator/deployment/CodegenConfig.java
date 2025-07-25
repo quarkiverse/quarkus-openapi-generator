7e6651a342d938272b297721ff007cd6b32285c8
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2025-07-25 10:07:48 -0300

    Add bean validation feature from apicurio-codegen 1.2.5.Final (#1256)

--- server/deployment/src/main/java/io/quarkiverse/openapi/server/generator/deployment/CodegenConfig.java
+++ server/deployment/src/main/java/io/quarkiverse/openapi/server/generator/deployment/CodegenConfig.java
@@ -14,6 +14,7 @@ public interface CodegenConfig extends ServerCodegenConfig {
     String INPUT_BASE_DIR = CODEGEN_TIME_CONFIG_PREFIX + ".input-base-dir";
     String CODEGEN_REACTIVE = CODEGEN_TIME_CONFIG_PREFIX + ".reactive";
     String GENERATE_BUILDERS = CODEGEN_TIME_CONFIG_PREFIX + ".builders";
+    String CODEGEN_BEAN_VALIDATION = CODEGEN_TIME_CONFIG_PREFIX + ".bean-validation";
 
     static String getBasePackagePropertyName() {
         return CODEGEN_BASE_PACKAGE;
@@ -33,5 +34,9 @@ public interface CodegenConfig extends ServerCodegenConfig {
 
     static String getGenerateBuilders() {
         return GENERATE_BUILDERS;
+    }
+
+    static String getUseBeanValidation() {
+        return CODEGEN_BEAN_VALIDATION;
     }
 }
