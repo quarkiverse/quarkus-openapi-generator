e5488955d638ecb2e9914ea5f18f5a1abd9b8a83
Michael Sonnleitner <michael.sonnleitner2@raiffeisen-software.at>
2025-10-23 19:35:09 +0200

    Support GAV coordinates for client generation #1340

--- client/integration-tests/gav/src/test/java/io/quarkiverse/openapi/generator/it/QuarkusGAVOpenApiTest.java
+++ client/integration-tests/gav/src/test/java/io/quarkiverse/openapi/generator/it/QuarkusGAVOpenApiTest.java
@@ -1,21 +1,23 @@
 package io.quarkiverse.openapi.generator.it;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class QuarkusGAVOpenApiTest {
     @Test
     void apiIsBeingGenerated() {
         assertThatCode(
-              () -> Class.forName("org.openapi.quarkus.io_quarkiverse_openapi_generator_quarkus_openapi_generator_gav_source_echo1.api.DefaultApi"))
-              .doesNotThrowAnyException();
+                () -> Class.forName(
+                        "org.openapi.quarkus.io_quarkiverse_openapi_generator_quarkus_openapi_generator_gav_source_echo1.api.DefaultApi"))
+                .doesNotThrowAnyException();
     }
 
     @Test
     void apiIsBeingNotGenerated() {
         assertThatCode(
-              () -> Class.forName("org.openapi.quarkus.io_quarkiverse_openapi_generator_quarkus_openapi_generator_gav_source_echo2.api.DefaultApi"))
-              .isInstanceOf(ClassNotFoundException.class);
+                () -> Class.forName(
+                        "org.openapi.quarkus.io_quarkiverse_openapi_generator_quarkus_openapi_generator_gav_source_echo2.api.DefaultApi"))
+                .isInstanceOf(ClassNotFoundException.class);
     }
 }
