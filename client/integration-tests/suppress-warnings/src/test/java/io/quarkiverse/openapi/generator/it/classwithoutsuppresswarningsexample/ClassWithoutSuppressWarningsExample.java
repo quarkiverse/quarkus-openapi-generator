1b2c08169a9d18ddd3b0a30e6db1568bbcc22f00
Ameer Abdallah <ameeryabdallah1@gmail.com>
2025-11-07 11:18:50 -0800

    [#1369] Add @SuppressWarnings to generated client classes (#1370)
    * [#1369] Add @SuppressWarnings to generated client classes
    
    * Fix openapi file extension

---
+++ client/integration-tests/suppress-warnings/src/test/java/io/quarkiverse/openapi/generator/it/classwithoutsuppresswarningsexample/ClassWithoutSuppressWarningsExample.java
@@ -0,0 +1,5 @@
+package io.quarkiverse.openapi.generator.it.classwithoutsuppresswarningsexample;
+
+// A class without @SuppressWarnings annotation for testing purposes. Do not delete!
+public class ClassWithoutSuppressWarningsExample {
+}
