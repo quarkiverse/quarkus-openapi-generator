55edf3d3531529671cd7f8618d10785118fdb35a
Christian Hauser <c.hauser@active.ch>
2026-01-09 17:42:24 +0100

    chore: improve media type to PascalCase converter

--- client/deployment/src/test/java/io/quarkiverse/openapi/generator/deployment/template/MediaTypeExtensionsTest.java
+++ client/deployment/src/test/java/io/quarkiverse/openapi/generator/deployment/template/MediaTypeExtensionsTest.java
@@ -8,8 +8,11 @@ class MediaTypeExtensionsTest {
 
     @Test
     void pascalCase() {
+        assertEquals("ApplicationJson", MediaTypeExtensions.pascalCase("application/json; charset=utf-8; version=1.1"));
         assertEquals("ApplicationJson", MediaTypeExtensions.pascalCase("application/json; charset=UTF-8"));
         assertEquals("ApplicationJson", MediaTypeExtensions.pascalCase("application/json"));
+        assertEquals("ApplicationXWwwFormUrlencoded", MediaTypeExtensions.pascalCase("application/x-www-form-urlencoded"));
+        assertEquals("ApplicationVndApiJson", MediaTypeExtensions.pascalCase("application/vnd.api+json"));
         assertEquals("ImageSvgXml", MediaTypeExtensions.pascalCase("image/svg+xml"));
         assertEquals("TextPlain", MediaTypeExtensions.pascalCase("text/plain"));
         assertEquals("", MediaTypeExtensions.pascalCase(""));
