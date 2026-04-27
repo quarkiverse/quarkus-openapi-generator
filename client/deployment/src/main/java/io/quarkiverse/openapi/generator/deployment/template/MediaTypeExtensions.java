59ec9f3118ccf629e8bb692b7af821d92e8f09e1
Rok Koleša <rok.kolesa@gmail.com>
2026-04-28 01:55:07 +0200

    fix: do not generate duplicate method signatures when method-per-media-type is enabled (#1519)

--- client/deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/template/MediaTypeExtensions.java
+++ client/deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/template/MediaTypeExtensions.java
@@ -3,6 +3,9 @@ package io.quarkiverse.openapi.generator.deployment.template;
 import static java.util.stream.Collectors.joining;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import io.quarkus.qute.TemplateExtension;
 
@@ -35,5 +38,19 @@ public class MediaTypeExtensions {
                 .map(word -> word.substring(0, 1).toUpperCase()
                         + word.substring(1).toLowerCase())
                 .collect(joining());
+    }
+
+    public static List<Map<String, String>> deduplicateByMediaType(List<Map<String, String>> consumes) {
+        if (consumes == null || consumes.isEmpty()) {
+            return consumes;
+        }
+        final Map<String, List<Map<String, String>>> mediaTypeGrouping = consumes
+                .stream()
+                .collect(Collectors.groupingBy(m -> pascalCase(m.get("mediaType"))));
+
+        // just retain the first 'consumes' based on mediaType to avoid duplicate method signatures
+        return mediaTypeGrouping.values().stream()
+                .map(mediaTypeGroup -> mediaTypeGroup.get(0))
+                .collect(Collectors.toList());
     }
 }
