25937f7d0f08ab049b4f47bbe717e144fac82a1f
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2026-05-19 16:24:31 -0300

    [manual-cherry-pick] Load openapi specifications from jar maven dependency (GAV) of project (#1631)
    * * Load openapi specifications from jar maven dependency (GAV) of project #1357
    
    - Added support for zipped (jar/zip) OpenAPI specifications.
    - Introduced `ZippedSpecInputModel` and `JarOrZipGAVCoordinateOpenApiSpecInputProvider`.
    - Updated `SpecItemConfig` and `CodegenConfig` to allow specification of multiple GAV-based files.
    - Reworked tests to include parameterized testing for multiple GAV sources.
    - Simplified YAML-based GAV provider to inherit from `AbstractGAVCoordinateOpenApiSpecInputProvider`.
    
    * added include-gavs option & docu
    
    * formating
    
    * review
    
    * review
    
    * doc added
    
    * review
    
    * review#2
    
    ---------
    
    Co-authored-by: Michael Sonnleitner <michael.sonnleitner2@raiffeisen-software.at>
    
    * Apply format
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>
    
    ---------
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>
    Co-authored-by: Michael Sonnleitner <65229544+michaelsonnleitner@users.noreply.github.com>
    Co-authored-by: Michael Sonnleitner <michael.sonnleitner2@raiffeisen-software.at>

---
+++ client/deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/GavItemConfig.java
@@ -0,0 +1,23 @@
+package io.quarkiverse.openapi.generator.deployment;
+
+import java.util.List;
+import java.util.Optional;
+
+import io.smallrye.config.WithDefault;
+import io.smallrye.config.WithName;
+
+/*
+ * Model for the configuration of this extension.
+ * It's used for documentation purposes only.
+ * The configuration is consumed in the codegen phase, before build time.
+ * Not meant to be used outside this scope.
+ * Config items can be applied only on gav
+ */
+public interface GavItemConfig {
+    /**
+     * List of OpenAPI spec files in GAV to be generated
+     */
+    @WithName("spec-files")
+    @WithDefault("openapi.yaml")
+    Optional<List<String>> gavSpecFiles();
+}
